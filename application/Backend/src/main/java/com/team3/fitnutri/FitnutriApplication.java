faaf5fc2a83b3cd35c11d6301d19c9e4e75e318c
Uzair Mohammed <uzair.hamed@gmail.com>
2024-07-07 21:17:44 -0700

    Added updated Backend directory

---
+++ application/Backend/src/main/java/com/team3/fitnutri/FitnutriApplication.java
@@ -0,0 +1,13 @@
+package com.team3.fitnutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnutriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnutriApplication.class, args);
+	}
+
+}
---
+++ application/Backend/src/test/java/com/team3/fitnutri/FitnutriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.fitnutri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnutriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3575058e2f61d1730639456c62174dd56061e7a6
Uzair Mohammed <uzair.hamed@gmail.com>
2024-07-22 19:27:11 -0700

    Added RecipeControllerIntegrationTest.java and updated SecurityConfig

--- application/Backend/src/test/java/com/team3/fitnutri/controllers/RecipeControllerIntegrationTest.java
+++ application/Backend/src/test/java/com/team3/fitnutri/controllers/RecipeControllerIntegrationTest.java
@@ -1,21 +1,16 @@
 package com.team3.fitnutri.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.team3.fitnutri.config.SecurityConfig;
 import com.team3.fitnutri.models.Recipe;
 import com.team3.fitnutri.services.RecipeService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;

14a6a2ea9cf99202a7559dc6e3fbd930e82c0977
MitCaine <99290881+MitCaine@users.noreply.github.com>
2024-07-24 23:40:36 -0700

    Updated header and footer

--- application/nutriapp/src/App.js
+++ application/nutriapp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
-/*import Footer from './Footer';*/
+import Footer from './Footer';
 import Main from './Main';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
@@ -22,37 +22,39 @@ import Discussion from './Discussion';
 import FitnessAssesment from './FitnessAssesment';
 import AssesmentWorkout from './AssesmentWorkout';
 import Article from './Article';
-
+import './App.css';
+import './Footer.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/HealthInfo" element={<HealthInfo />} />
-          <Route path="/Progress" element={<Progress />} />
-          <Route path="/contact" element={<Contact />} />  
-          <Route path="/nutrition" element={<Nutrition />} /> 
-          <Route path="/fitness" element={<Fitness />} />
-          <Route path="/PersonalNutritionPlan" element={<PersonalNutritionPlan />} />
-          <Route path="/PersonalRecipes" element={<PersonalRecipes />} />
-          <Route path="/RecipeVideo" element={<RecipeVideo />} />
-          <Route path="/PersonalWorkoutPlan" element={<PersonalWorkoutPlan />} />
-          <Route path="/PersonalWorkouts" element={<PersonalWorkouts />} />
-          <Route path="/WorkoutVideo" element={<WorkoutVideo />} />
-          <Route path="/NutriCommunity" element={<NutriCommunity />} />
-          <Route path="/Discussion" element={<Discussion />} />
-          <Route path="/Article" element={<Article />} />
-          <Route path="/FitnessAssesment" element={<FitnessAssesment />} />
-          <Route path="/AssesmentWorkout" element={<AssesmentWorkout />} />
-
-        </Routes>
-       {/*<Footer />*/}
+      <div className="app-container">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/HealthInfo" element={<HealthInfo />} />
+            <Route path="/Progress" element={<Progress />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/nutrition" element={<Nutrition />} />
+            <Route path="/fitness" element={<Fitness />} />
+            <Route path="/PersonalNutritionPlan" element={<PersonalNutritionPlan />} />
+            <Route path="/PersonalRecipes" element={<PersonalRecipes />} />
+            <Route path="/RecipeVideo" element={<RecipeVideo />} />
+            <Route path="/PersonalWorkoutPlan" element={<PersonalWorkoutPlan />} />
+            <Route path="/PersonalWorkouts" element={<PersonalWorkouts />} />
+            <Route path="/WorkoutVideo" element={<WorkoutVideo />} />
+            <Route path="/NutriCommunity" element={<NutriCommunity />} />
+            <Route path="/Discussion" element={<Discussion />} />
+            <Route path="/Article" element={<Article />} />
+            <Route path="/FitnessAssesment" element={<FitnessAssesment />} />
+            <Route path="/AssesmentWorkout" element={<AssesmentWorkout />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );

fbd69dcc238677827942bb1ec7c67824ca601833
Uzair Mohammed <uzair.hamed@gmail.com>
2024-07-28 22:06:34 -0700

    Test CORS edit for UsersController pt3

--- application/Backend/src/main/java/com/team3/fitnutri/controllers/UsersController.java
+++ application/Backend/src/main/java/com/team3/fitnutri/controllers/UsersController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:80")
+@CrossOrigin()
 @RequestMapping("/user")
 public class UsersController {
     @Autowired

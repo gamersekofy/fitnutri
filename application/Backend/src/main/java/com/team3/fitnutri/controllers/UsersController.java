7a405f6f898efedbc8700a47dc64e38379fac84e
eduardo <edmunoz1030@gmail.com>
2024-07-29 20:38:15 -0700

    testing

--- application/Backend/src/main/java/com/team3/fitnutri/controllers/UsersController.java
+++ application/Backend/src/main/java/com/team3/fitnutri/controllers/UsersController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UsersController {
     @Autowired
     private UserService userService;
